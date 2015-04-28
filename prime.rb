f9118e100e0258d1988eb2e71d18d5b53cc54ded
yuki nara <yuki_nara@icloud.com>
2015-04-28 20:41:15 +0900

    Update prime.rb

--- prime.rb
+++ prime.rb
@@ -1,8 +1,26 @@
-require 'prime'
-
 class PrimeChecker
   def self.prime?(num)
-    Prime.instance.prime?(num)
+    arr = (2..num-1).to_a
+    i = 0
+    if num % arr[i] == 0
+      puts "false"
+    elsif num % num-1 != 0
+    puts "true"
+    elsif i << + 1
+      num % arr[i]
+    return
+    end
   end
-  puts self.prime?(109273)
+  puts self.prime?(101)
 end
+
+
+
+# require 'prime'
+
+# class PrimeChecker
+  # def self.prime?(num)
+    # Prime.instance.prime?(num)
+  # end
+  # puts self.prime?(109273)
+# end

258d1d9e827e28c1267697082f2c63233fd5f02d
yuki nara <yuki_nara@icloud.com>
2015-04-29 15:52:16 +0900

    ループを追加

--- 20150429.rb
+++ 20150429.rb
@@ -3,7 +3,7 @@ class Extracter
     value = arr.sort_by{|a| -a}
     k = [arr.index(value[0]),arr.index(value[1]),arr.index(value[2])]
     v = [value[0],value[1],value[2]]
-    Hash[k.zip(v).map { |k,v| [k,v] }]
+    Hash[k.zip(v).each { |k,v| [k,v] }]
   end
   puts self.print_idx_and_value([1,2,3,4,5])
 end

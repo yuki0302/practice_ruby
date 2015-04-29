98503ba023548c62fbb8bd726b3634c12d4fd901
yuki0302 <yuki_nara@icloud.com>
2015-04-29 15:48:21 +0900

    map追加

--- 20150429.rb
+++ 20150429.rb
@@ -1,12 +1,9 @@
 class Extracter
   def self.print_idx_and_value(arr)
-    value = arr.sort_by{|i| -i}
-    {arr.index(value[0]) => value[0], arr.index(value[1]) => value[1], arr.index(value[2]) => value[2]}
+    value = arr.sort_by{|a| -a}
+    k = [arr.index(value[0]),arr.index(value[1]),arr.index(value[2])]
+    v = [value[0],value[1],value[2]]
+    Hash[k.zip(v).map { |k,v| [k,v] }]
   end
-  # 問1
-  puts self.print_idx_and_value([10, 1, 2, 5, 99, 0, 3])
-  # 問2
-  puts self.print_idx_and_value([1, 2, 3, 4, 5])
-  # 問3
-  puts self.print_idx_and_value([5, 4, 3, 2, 1])
+  puts self.print_idx_and_value([1,2,3,4,5])
 end

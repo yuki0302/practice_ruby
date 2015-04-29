3dfaa80d5477c4def8d02fe72eb8402aa9360798
yuki nara <yuki_nara@icloud.com>
2015-04-29 16:47:52 +0900

    Update 20150429.rb

--- 20150429.rb
+++ 20150429.rb
@@ -1,9 +1,12 @@
 class Extracter
   def self.print_idx_and_value(arr)
     value = arr.sort_by{|a| -a}
-    k = [arr.index(value[0]),arr.index(value[1]),arr.index(value[2])]
+    array = []
+    (0..2).each do |i|
+      array << arr.index(value[i])
+    end
     v = value.first(3)
-    Hash[k.zip(v).each { |k,v| [k,v] }]
+    Hash[array.zip(v).each { |k,v| [k,v] }]
   end
   puts self.print_idx_and_value([10, 1, 2, 5, 99, 0, 3])
 end
